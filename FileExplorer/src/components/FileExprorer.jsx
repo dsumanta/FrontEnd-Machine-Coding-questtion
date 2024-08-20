24e7deb6f7cb27d15e6ab145f8732c4ccd230f1e
Sumanta Das <2007832@kfintech.com>
2024-08-20 15:33:44 +0530

    Insert file and folder is done

--- FileExplorer/src/components/FileExprorer.jsx
+++ FileExplorer/src/components/FileExprorer.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
-function FileExprorer({ fileData }) {
+function FileExprorer({ fileData,handleInsertNode=()=>{} }) {
   const [openExplorer, setOpneExplore] = useState(false);
+  const [showInput,setShowInput] = useState({
+    visibility:false,
+    isFolder:false
+  })
+  const onFolderCreate = (e,isFolder)=>{
+    e.stopPropagation()
+    setOpneExplore(true)
+    setShowInput({...showInput,visibility:true,isFolder:isFolder})
+  }
+  const addFolder = (e)=>{
+      if(e.keyCode===13 && e.target.value){
+        handleInsertNode(fileData.id,e.target.value,showInput.isFolder)
+        setShowInput({...showInput,visibility:false})
+        
+      }
+  }
   return (
     <div className=" cursor-pointer">
       {fileData.isFolder ? (
@@ -12,16 +28,30 @@ function FileExprorer({ fileData }) {
           >
             📁 {fileData.name}
             <span className=" m-6">
-              <button className=" mx-2 p-1 bg-white">File +</button>
-              <button className=" mx-2 p-1 bg-white">Folder +</button>
+              <button onClick={(e)=>onFolderCreate(e,false)} className=" mx-2 p-1 bg-white">File +</button>
+              <button  onClick={(e)=>onFolderCreate(e,true)} className=" mx-2 p-1 bg-white">Folder +</button>
             </span>
           </div>
 
           <div className={` ms-5 ${openExplorer ? "block" : "hidden"}`}>
+            {
+              showInput.visibility && ( <div>
+                 <span>
+                  {showInput.isFolder?"📁":"📄"}
+                  <input 
+                   type="text"
+                   className=" "
+                   autoFocus
+                   onKeyDown={addFolder}
+                   onBlur={()=>setShowInput({...showInput,visibility:false})}
+                   />
+                 </span>
+              </div> )
+            }
             {fileData.items.map((items, index) => {
               return (
                 <div key={index}>
-                  <FileExprorer fileData={items} />
+                  <FileExprorer fileData={items} handleInsertNode={handleInsertNode}  />
                 </div>
               );
             })}
@@ -29,7 +59,7 @@ function FileExprorer({ fileData }) {
         </div>
       ) : (
         <div className=" bg-red-300 px-4 py-1 w-fit border border-spacing-1">
-          🗒️ {fileData.name}
+          🗒️  {fileData.name}
         </div>
       )}
     </div>
